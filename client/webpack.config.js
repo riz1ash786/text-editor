c78526e958658e8d3c80b55671375efb3358ab80
Rizwan Ashraf <riz1ash786@gmail.com>
2022-05-12 15:56:29 +0100

    manifest plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,6 +27,26 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js",
       }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description:
+          "create a text editor that runs in the browser and also functions offline",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
