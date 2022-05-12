761506a4a6297f4c289d598e51b0d425208fdf15
Rizwan Ashraf <riz1ash786@gmail.com>
2022-05-12 15:44:45 +0100

    start install code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,17 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Store triggered events
+  window.deferredPrompt = event;
+  // Remove hidden class from button.
+  butInstall.classList.toggle("hidden", false);
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  const promptEvent = window.deferredPrompt;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  if (!promptEvent) {
+    return;
+  }
+
