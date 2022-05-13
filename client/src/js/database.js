4c7aea887f3c4ae85082812cc4d535b559aeda92
Rizwan Ashraf <riz1ash786@gmail.com>
2022-05-13 16:09:25 +0100

    console log and npm i in client

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,6 +15,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+  console.log("putting into db");
   const jateDB = await openDB("jate", 1);
   const tx = jateDB.transaction("jate", "readwrite");
   const store = tx.objectStore("jate");
@@ -25,6 +26,7 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async (e) => {
+  console.log("getting from db");
   const jateDb = await openDB("jate", 1);
   const tx = jateDb.transaction("jate", "readonly");
   const store = tx.objectStore("jate");
